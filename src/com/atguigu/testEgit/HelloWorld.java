9812be455fba58670a99ac57c0d2fa3a82d6c539
chendongxu <chendongxu@chendongxu-PC>
2016-08-24 22:53:38 +0800

    test 01

--- src/com/atguigu/testEgit/HelloWorld.java
+++ src/com/atguigu/testEgit/HelloWorld.java
@@ -3,6 +3,8 @@ package com.atguigu.testEgit;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Update 01");
+		 System.out.println("Update 01");
+		 System.out.println("test 01");
+		
 	}
 }
